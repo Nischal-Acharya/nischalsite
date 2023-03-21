3ec37fbf096ab520528d44b4e740938814075fb4
NischalHost <125393262+NischalHost@users.noreply.github.com>
2023-03-21 11:52:25 +0545

    Update script.js
    Changed gmail

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function sendMail() {
   // create a new element to represent the file
   var element = document.createElement('a');
   // set the file's URL and name
-  element.setAttribute('href', 'mailto: nishchalacharyaofficial@gmail.com');
+  element.setAttribute('href', 'mailto: nismsg1@gmail.com');
   
   // simulate a click on the new element to start the download
   element.style.display = 'none';
