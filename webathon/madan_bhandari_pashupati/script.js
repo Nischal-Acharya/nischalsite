d3b1714b971fa1a94587ae18e1783ec87399b469
NischalHost <125393262+NischalHost@users.noreply.github.com>
2024-12-02 21:10:37 +0545

    Add files via upload

---
+++ webathon/madan_bhandari_pashupati/script.js
@@ -0,0 +1,22 @@
+
+// This navigationToggle() Fuction will be used to collapse the navigation
+
+function navigationToggle() {
+
+    var toggleOpen = document.getElementById('toggleOpen');
+    var toggleClose = document.getElementById('toggleClose');
+    var collapseMenu = document.getElementById('collapseMenu');
+
+    function handleClick() {
+        if (collapseMenu.style.display === 'block') {
+            collapseMenu.style.display = 'none';
+        } else {
+            collapseMenu.style.display = 'block';
+        }
+    }
+
+    toggleOpen.addEventListener('click', handleClick);
+    toggleClose.addEventListener('click', handleClick);
+}
+
+navigationToggle()
